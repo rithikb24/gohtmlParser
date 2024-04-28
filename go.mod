88dd369d5fd370d9e40bf86bf558f1640dc48dfc
rithikb24 <rithik.bhandari@smallcase.com>
2024-04-28 15:30:46 +0530

    feat: basic parsing implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rithikb24/gohtmlParser
 
 go 1.21.0
+
+require golang.org/x/net v0.24.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hello world")
+	filePath := "ex3.html"
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := string(file)
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
 }
